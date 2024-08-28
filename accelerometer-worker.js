77b6728a3532b9a9916495b0d1cf70debfaaf51a
Bogdan Bujor <bogdan.bujor08@gmail.com>
2024-08-28 23:09:54 +0300

    fix: collecting var twice in code

--- accelerometer-worker.js
+++ accelerometer-worker.js
@@ -1,16 +1,16 @@
-let collecting = false;
+let collecting-worker = false;
 
 self.onmessage = function(e) {
     if (e.data.command === 'start') {
-        collecting = true;
+        collecting-worker = true;
         collectData();
     } else if (e.data.command === 'stop') {
-        collecting = false;
+        collecting-worker = false;
     }
 };
 
 function collectData() {
-    if (collecting) {
+    if (collecting-worker) {
         if ('DeviceMotionEvent' in self) {
             self.addEventListener('devicemotion', (event) => {
                 const { x, y, z } = event.accelerationIncludingGravity;
